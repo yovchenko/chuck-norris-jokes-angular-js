b8b19b5cdfc46f3ef1d0f548b9d4fc0a6678a022
yovchenko <yovchenko.w@gmail.com>
2018-04-06 13:36:27 +0300

    changed service function

--- src/app/app.js
+++ src/app/app.js
@@ -13,18 +13,24 @@ const MODULE_NAME = 'app';
 
 angular.module(MODULE_NAME, [])
   .directive('app', app)
-  .controller('AppCtrl', function($scope, $q, $http, $timeout) {
-    $http.get('https://api.icndb.com/jokes/random?escape=javascript')
-  .then(function (response) {
-    $scope.joke = response.data.value.joke;
-},function (error){
-    $scope.joke = "Sorry,no jokes today!"
-});
-$http.get('https://api.icndb.com/categories')
-  .then(function (response) {
-  $scope.categories= response.data.value;
-  },function (error){
-  $scope.categories = "Buttons"
-});
-});
+  .service('getData', function () {
+    this.req = function ($http, url) {
+      return $http.get(url)
+        .then(function (response) {
+          return response.data;
+        });
+    }
+  })
+  .controller('AppCtrl', function ($scope, $q, $http, getData) {
+    getData.req($http, 'http://api.icndb.com/jokes/random?escape=javascript').then(function (data) {
+      $scope.joke = data.value.joke;
+    }, function () {
+      $scope.joke = 'Oops,something went wrong!'
+    })
+    getData.req($http, 'https://api.icndb.com/categories').then(function (data) {
+      $scope.categories = data.value;
+    }, function () {
+      $scope.categories = 'Oops,something went wrong!'
+    })
+  });
 export default MODULE_NAME;
