0ece089898be4ea361139e23df87dfb39e50f492
yovchenko <yovchenko.w@gmail.com>
2018-04-06 12:02:53 +0300

    service.js has been created

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,6 @@
 import angular from 'angular';
-
 import '../style/app.css';
-
+import 'bootstrap/dist/css/bootstrap.css';
 let app = () => {
   return {
     template: require('./app.html'),
@@ -10,16 +9,22 @@ let app = () => {
   }
 };
 
-class AppCtrl {
-  constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
-  }
-}
-
 const MODULE_NAME = 'app';
 
 angular.module(MODULE_NAME, [])
   .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
-
+  .controller('AppCtrl', function($scope, $q, $http, $timeout) {
+    $http.get('https://api.icndb.com/jokes/random?escape=javascript')
+  .then(function (response) {
+    $scope.joke = response.data.value.joke;
+},function (error){
+    $scope.joke = "Sorry,no jokes today!"
+});
+$http.get('https://api.icndb.com/categories')
+  .then(function (response) {
+  $scope.categories= response.data.value;
+  },function (error){
+  $scope.categories = "Buttons"
+});
+});
 export default MODULE_NAME;
