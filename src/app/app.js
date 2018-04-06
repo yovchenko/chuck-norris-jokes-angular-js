2f4b19daa2ae13120b7074b6565cb59902cfdb08
yovchenko <yovchenko.w@gmail.com>
2018-04-06 15:35:18 +0300

    added eslint

--- src/app/app.js
+++ src/app/app.js
@@ -1,40 +1,40 @@
 import angular from 'angular';
 import '../style/app.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import '../../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import '../../node_modules/bootstrap/dist/js/bootstrap.min.js';
 let app = () => {
-  return {
-    template: require('./app.html'),
-    controller: 'AppCtrl',
-    controllerAs: 'app'
-  }
+	return {
+		template: require('./app.html'),
+		controller: 'AppCtrl',
+		controllerAs: 'app'
+	};
 };
 
 const MODULE_NAME = 'app';
 
 angular.module(MODULE_NAME, [])
-  .directive('app', app)
-  .service('getData', function () {
-    this.req = function ($http, url) {
-      return $http.get(url)
-        .then(function (response) {
-          return response.data;
-        });
-    }
-  })
-  .controller('AppCtrl', function ($scope, $q, $http, getData) {
-    getData.req($http, 'http://api.icndb.com/jokes/random?escape=javascript').then(function (data) {
-      $scope.joke = data.value.joke;
-    }, function () {
-      $scope.joke = 'Oops,something went wrong!'
-    })
-    getData.req($http, 'https://api.icndb.com/categories').then(function (data) {
-      $scope.categories = data.value;
-    }, function () {
-      $scope.categories = 'Oops,something went wrong!'
-    })
-    $scope.selectCategory = function(el) {
-     console.log(el.item);
-    }
-  });
+	.directive('app', app)
+	.service('getData', function () {
+		this.req = function ($http, url) {
+			return $http.get(url)
+				.then(function (response) {
+					return response.data;
+				});
+		};
+	})
+	.controller('AppCtrl', function ($scope, $q, $http, getData) {
+		getData.req($http, 'http://api.icndb.com/jokes/random?escape=javascript').then(function (data) {
+			$scope.joke = data.value.joke;
+		}, function () {
+			$scope.joke = 'Oops,something went wrong!';
+		});
+		getData.req($http, 'https://api.icndb.com/categories').then(function (data) {
+			$scope.categories = data.value;
+		}, function () {
+			$scope.categories = 'Oops,something went wrong!';
+		});
+		$scope.selectCategory = function(el) {
+			console.log(el.item);
+		};
+	});
 export default MODULE_NAME;
