05d0fe4ab7cf67bf28df27a8fcf9d0d98973db12
yovchenko <yovchenko.w@gmail.com>
2018-04-09 19:51:28 +0300

    changed unit test configuration

--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -2,19 +2,21 @@ import app from './app';
 
 describe('app', () => {
 
-  describe('AppCtrl', () => {
-    let ctrl;
+	describe('AppCtrl', () => {
+		var $controller;
 
-    beforeEach(() => {
-      angular.mock.module(app);
+		beforeEach(() => {
+			angular.mock.module(app);
 
-      angular.mock.inject(($controller) => {
-        ctrl = $controller('AppCtrl', {});
-      });
-    });
+			angular.mock.inject((_$controller_) => {
+				$controller = _$controller_;
+			});
+		});
 
-    it('should contain the starter url', () => {
-      expect(ctrl.url).toBe('https://github.com/preboot/angular-webpack');
-    });
-  });
+		it('should', () => {
+			var $scope = {};
+			var controller = $controller('AppCtrl', { $scope: $scope });
+			expect($scope.alertWarning).toEqual(false);
+		});
+	});
 });
