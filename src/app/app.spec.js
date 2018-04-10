bac0871b61b6e15c00756e7f21f36552c888a264
yovchenko <yovchenko.w@gmail.com>
2018-04-10 13:50:01 +0300

    unit test has been created

--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -3,13 +3,17 @@ import app from './app';
 describe('app', () => {
 
 	describe('AppCtrl', () => {
-		let $controller, $rootScope, createController;
+		let service, $controller, $rootScope, $http, $httpBackend, createController;
 
 		beforeEach(() => {
 			angular.mock.module(app);
 			angular.mock.inject(($injector) => {
+				service = $injector.get('getData');
 				$controller = $injector.get('$controller');
 				$rootScope = $injector.get('$rootScope');
+				$http = $injector.get('$http');
+				$httpBackend = $injector.get('$httpBackend');
+				$httpBackend.expectGET('https://api.icndb.com/jokes/random/1?escape=javascript').respond('Hi!');
 				createController = function () {
 					return $controller('AppCtrl', {
 						'$scope': $rootScope,
@@ -37,5 +41,11 @@ describe('app', () => {
 				expect($rootScope.alertWarning).toEqual(true);
 			}
 		});
+		it( 'Verify tourService.start has been called', function() {
+			service.req($http,'https://api.icndb.com/jokes/random/1?escape=javascript').then(function(data) {
+				expect(data).toEqual('Hi!');
+			});
+			$httpBackend.flush();
+		} );
 	});
 });
